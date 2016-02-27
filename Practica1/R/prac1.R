2a5b1c0ffbaedba0eb4d1844f5a19a5f59385b3a
cristinazuhe <zuhe18@gmail.com>
2016-02-27 23:02:20 +0100

    hasta 4.2.7 a falta de retocar y confirmar

--- Practica1/R/prac1.R
+++ Practica1/R/prac1.R
@@ -1,4 +1,7 @@
-#EJERCICIO 4.2.1
+
+###################################################
+##################EJERCICIO 4.2.1##################
+###################################################
 simula_unif <- function(N=3, dim=5, rang=0:9) {
   if(N<0){                                                  #Si paso tamaño de la lista negativo, no creo lista con valores buenos. 
     print("La lista no puede tener un tamaño negativo.")
@@ -19,7 +22,9 @@ simula_unif <- function(N=3, dim=5, rang=0:9) {
   return(mi_lista)
 }
 
-#EJERCICIO 4.2.2
+###################################################
+##################EJERCICIO 4.2.2##################
+###################################################
 simula_gaus <- function(N=3, dim=5, sigma=1:9){
   if(N<0){                                                  #Si paso tamaño de la lista negativo, no creo lista con valores buenos. 
     print("La lista no puede tener un tamaño negativo.")
@@ -43,10 +48,12 @@ simula_gaus <- function(N=3, dim=5, sigma=1:9){
   return(mi_lista)
 }
 
-#EJERCICIO 4.2.3
+###################################################
+##################EJERCICIO 4.2.3##################
+###################################################
 #Genero la lista de vectores con la funcion y los valores que nos indican
 N=50
-dim=5
+dim=2
 lista3 = simula_unif(N,dim,-50:50)
 
 #Creo un vector con los valores y (valores aleatorios generados).
@@ -60,4 +67,189 @@ for(j in 1:N){
      lista33x = c(lista33x, j)
   }
 } 
-plot(lista33x, lista33y)
+plot(lista33x, lista33y, xlab= "índice en lista", ylab="valor aleatorio uniforme", 
+     main = "Valores función uniforme",col="purple")
+
+###################################################
+##################EJERCICIO 4.2.4##################
+###################################################
+#Genero la lista de vectores con la funcion y los valores que nos indican
+N=50
+dim=2
+lista4 = simula_gaus(N,dim,5:7)
+
+#Creo un vector con los valores y (valores aleatorios generados).
+#Creo un vector con los valores x (indice de lista del valor).
+lista42y = NULL #en lista42y tendre todos los valores y.
+lista42x = NULL #en lista42y tendre todos los valores x.
+for(j in 1:N){
+  for(i in 1:dim){
+    lista41 = lista4[[j]][i]
+    lista42y = c(lista42y, lista41)
+    lista42x = c(lista42x, j)
+  }
+} 
+plot(lista42x,lista42y, xlab= "índice en lista", ylab="valor aleatorio gaussiano", 
+     main = "Valores función gaussiana", col="orange")
+
+
+###################################################
+##################EJERCICIO 4.2.5##################
+###################################################
+simula_recta <- function(intervalo=-50:50, a=NA){
+  #Tomo dos puntos aleatorios del cuadrado con un aleatorio
+  primerx = sample(intervalo,1)
+  primery = sample(intervalo,1)
+  segundox = sample(intervalo, 1)
+  while(segundox == primerx){ #Evito la división por 0
+    segundox=sample(intervalo,1)
+  }
+  segundoy = sample(intervalo, 1)
+  
+  #Añado los dos puntos del cuadrado para dibujarlos
+  x=c(primerx, segundox) 
+  y=c(primery, segundoy)
+  
+  #Calculo a, b que definen la recta que pasa por los dos puntos
+  a= ((segundoy - primery)/(segundox - primerx))
+  b= (primery - (a*primerx))
+  
+  #Represento el cuadrado de trabajo, los puntos y la recta que los une
+  plot(x,y, col= "red", 
+       xlim = c(intervalo[1], intervalo[length(intervalo)]), 
+       ylim = c(intervalo[1], intervalo[length(intervalo)])) 
+  abline(b,a) # Recta ax+b (pendiente a)(corte b)
+
+  val=c(a,b)
+  return(val) #devuelvo los coeficientes a y b
+}
+
+###################################################
+##################EJERCICIO 4.2.6##################
+###################################################
+#Genero la recta que corta al cuadrado
+val = simula_recta(-50:50)
+#Obtengo los coeficientes de la recta
+a = val[1]
+b = val[2]
+
+#Genero los datos aleatorios uniformes y los represento junto a la recta
+N=50
+dim=2
+lista6 = simula_unif(N,dim,-50:50)
+lista61y = NULL #en lista61y tendre todos los valores y.
+lista61x = NULL #en lista61y tendre todos los valores x.
+for(j in 1:N){
+  for(i in 1:dim){
+    lista62 = lista6[[j]][i]
+    lista61y = c(lista61y, lista62)
+    lista61x = c(lista61x, j)
+  }
+}
+
+lista61xpos = NULL
+lista61ypos = NULL
+lista61xneg = NULL
+lista61yneg = NULL
+for(k in 1:length(lista61x)){
+  num = lista61y[k] -a*lista61x[k] -b
+  if(num>0){
+    lista61xpos = c(lista61xpos, lista61x[k])
+    lista61ypos = c(lista61ypos, lista61y[k])
+  }
+  if(num<0){
+    lista61xneg = c(lista61xneg, lista61x[k])
+    lista61yneg = c(lista61yneg, lista61y[k])
+  }
+}
+
+points(lista61xpos,lista61ypos, col = "purple")
+points(lista61xneg,lista61yneg, col = "green")
+
+###################################################
+##################EJERCICIO 4.2.7##################
+###################################################
+lista71xpos1 = NULL
+lista71ypos1 = NULL
+lista71xneg1 = NULL
+lista71yneg1 = NULL
+lista71xpos2 = NULL
+lista71ypos2 = NULL
+lista71xneg2 = NULL
+lista71yneg2 = NULL
+lista71xpos3 = NULL
+lista71ypos3 = NULL
+lista71xneg3 = NULL
+lista71yneg3 = NULL
+lista71xpos4 = NULL
+lista71ypos4 = NULL
+lista71xneg4 = NULL
+lista71yneg4 = NULL
+for(k in 1:length(lista61x)){
+  num1 = (lista61x[k] - 10)^2 + (lista61y[k] - 20)^2 -400
+  num2 = 0.5*(lista61x[k] + 10)^2 + (lista61y[k] - 20)^2 -400
+  num3 = 0.5*(lista61x[k] - 10)^2 - (lista61y[k] + 20)^2 -400
+  num4 = lista61y[k] - 20*((lista61x[k])^2) - 5*(lista61x[k]) +3
+  #Para primera funcion
+  if(num1>0){
+    lista71xpos1 = c(lista71xpos1, lista61x[k])
+    lista71ypos1 = c(lista71ypos1, lista61y[k])
+  }
+  else if(num1<0){
+    lista71xneg1 = c(lista71xneg1, lista61x[k])
+    lista71yneg1 = c(lista71yneg1, lista61y[k])
+  }
+  #Para segunda funcion
+  if(num2>0){
+    lista71xpos2 = c(lista71xpos2, lista61x[k])
+    lista71ypos2 = c(lista71ypos2, lista61y[k])
+  }
+  else if(num2<0){
+    lista71xneg2 = c(lista71xneg2, lista61x[k])
+    lista71yneg2 = c(lista71yneg2, lista61y[k])
+  }
+  #Para tercera funcion
+  if(num3>0){
+    lista71xpos3 = c(lista71xpos3, lista61x[k])
+    lista71ypos3 = c(lista71ypos3, lista61y[k])
+  }
+  else if(num3<0){
+    lista71xneg3 = c(lista71xneg3, lista61x[k])
+    lista71yneg3 = c(lista71yneg3, lista61y[k])
+  }
+  #Para cuarta funcion
+  if(num4>0){
+    lista71xpos4 = c(lista71xpos4, lista61x[k])
+    lista71ypos4 = c(lista71ypos4, lista61y[k])
+  }
+  else if(num4<0){
+    lista71xneg4 = c(lista71xneg4, lista61x[k])
+    lista71yneg4 = c(lista71yneg4, lista61y[k])
+  }
+}
+
+plot(lista71xpos1,lista71ypos1, col = "purple",
+     xlim = c(-50, 50), 
+     ylim = c(-50, 50), main = "Primera funcion")
+points(lista71xneg1,lista71yneg1, col = "green")
+
+plot(lista71xpos2,lista71ypos2, col = "purple",
+     xlim = c(-50, 50), 
+     ylim = c(-50, 50), main = "Segunda funcion")
+points(lista71xneg2,lista71yneg2, col = "green")
+
+plot(lista71xpos3,lista71ypos3, col = "purple",
+     xlim = c(-50, 50), 
+     ylim = c(-50, 50), main = "Tercera funcion")
+points(lista71xneg3,lista71yneg3, col = "green")
+
+plot(lista71xpos4,lista71ypos4, col = "purple",
+     xlim = c(-50, 50), 
+     ylim = c(-50, 50), main = "Cuarta funcion")
+points(lista71xneg4,lista71yneg4, col = "green")
+
+
+###################################################
+##################EJERCICIO 4.2.8##################
+###################################################
+
